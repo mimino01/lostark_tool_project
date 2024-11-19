995bf22c74d87b8a0bc435e4aa8cfca304e64561
DESKTOP-18S0CFC\mimin <mimino0144@gmail.com>
2024-11-19 14:42:13 +0900

    데이터 정제용 클래스 작성

--- server/LosrarkProjectServer/src/main/java/org/example/Main.java
+++ server/LosrarkProjectServer/src/main/java/org/example/Main.java
@@ -1,9 +1,7 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +9,6 @@ public class Main {
 //        socketConnecter.socket();
         int[] categoryCodes = {90200, 90300, 90400, 90500, 90600, 90700};
 
-        // Fetching data for each category code
         List<Map<String, Object>> jsonDatas = new ArrayList<>();
         Map<String, Object> jsonData;
         for (int categoryCode : categoryCodes) {
@@ -24,11 +21,146 @@ public class Main {
 
         GetLostarkData getLostarkData = new GetLostarkData();
         List<List<Map<String, Object>>> data = GetLostarkData.getData(jsonDatas);
+        List<Map<String, Object>> SortedUsefulData = new ArrayList<>();
+        DataSheet dataSheet = new DataSheet();
 
         for (List<Map<String, Object>> dataList : data) {
             for (Map<String, Object> dataMap : dataList) {
-                System.out.println(dataMap);
+//                System.out.println(dataMap);
+                List<String> keyToFind = Arrays.asList("Id","Name","YDayAvgPrice","RecentPrice");
+
+                Map<String, Object> usefulData = keyToFind.stream()
+                        .filter(dataMap::containsKey)
+                        .collect(Collectors.toMap(key -> key, dataMap::get));
+
+                usefulData.forEach(dataSheet::addData);
             }
+        }
+        for (Map<String, Object> dataMap : dataSheet.getData()) {
+            System.out.println(dataMap);
+        }
+    }
+
+    public static class DataSheet {
+        public static class Data {
+            String id;
+            String name;
+            String YesterdayAvgPrice;
+            String RecentPrice;
+            int state;
+            public Data() {
+                this.id = null;
+                this.name = null;
+                this.YesterdayAvgPrice = null;
+                this.RecentPrice = null;
+                this.state = 0;
+            }
+
+            public void clean () {
+                this.id = null;
+                this.name = null;
+                this.YesterdayAvgPrice = null;
+                this.RecentPrice = null;
+                this.state = 0;
+            }
+
+            public int getDataState () {
+                state = 0;
+                if (YesterdayAvgPrice != null) {
+                    state += 1;
+                }
+                if (RecentPrice != null) {
+                    state += 1;
+                }
+                if (name != null) {
+                    state += 1;
+                }
+                if (id != null) {
+                    state += 1;
+                }
+                return state;
+            }
+
+            public Map<String, Object> getData() {
+                Map<String, Object> data = new HashMap<>();
+                data.put("Id",id);
+                data.put("Name",name);
+                data.put("YdayAvgPrice",YesterdayAvgPrice);
+                data.put("RecentPrice",RecentPrice);
+                return data;
+            }
+
+            public String getId() {
+                return id;
+            }
+
+            public void setId(String id) {
+                this.id = id;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public String getYesterdayAvgPrice() {
+                return YesterdayAvgPrice;
+            }
+
+            public void setYesterdayAvgPrice(String yesterdayAvgPrice) {
+                YesterdayAvgPrice = yesterdayAvgPrice;
+            }
+
+            public String getRecentPrice() {
+                return RecentPrice;
+            }
+
+            public void setRecentPrice(String recentPrice) {
+                RecentPrice = recentPrice;
+            }
+        }
+        List<Data> data;
+        Data tempData;
+
+        public DataSheet() {
+            data = new ArrayList<>();
+            tempData = new Data();
+        }
+
+        public void addData(String key, Object value) {
+//            System.out.println(key + " : " + value + " : " + tempData.getDataState());
+            if (tempData == null) {
+                tempData = new Data();
+            }
+            if (tempData.getDataState() >= 4) {
+                data.add(tempData);
+                tempData = new Data();
+            }
+            switch (key) {
+                case "Id":
+                    tempData.setId(String.valueOf(value));
+                    break;
+                case "Name":
+                    tempData.setName((String) value);
+                    break;
+                case "YDayAvgPrice":
+                    tempData.setYesterdayAvgPrice(String.valueOf(value));
+                    break;
+                case "RecentPrice":
+                    tempData.setRecentPrice(String.valueOf(value));
+                    break;
+            }
+        }
+
+        public List<Map<String, Object>> getData() {
+            List<Map<String, Object>> response = new ArrayList<>();
+            for (Data data : data) {
+                response.add(data.getData());
+            }
+            return response;
         }
     }
 }
